05740c125ea4c3e4d18f41e61d3154c97555fc09
amoretto <amoretto@outlook.com.br>
2020-11-14 16:40:52 -0300

    Mod 1 Class 4

--- PrjModulo1/Form1.Designer.cs
+++ PrjModulo1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PrjModulo1
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -30,44 +30,140 @@
         {
             this.btnButtonTwo = new System.Windows.Forms.Button();
             this.btnButtonOne = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblCopy = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblNameTxtBox = new System.Windows.Forms.Label();
+            this.lblNameOut = new System.Windows.Forms.Label();
+            this.lblNameList = new System.Windows.Forms.Label();
+            this.lstNames = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // btnButtonTwo
             // 
-            this.btnButtonTwo.Location = new System.Drawing.Point(99, 141);
+            this.btnButtonTwo.Font = new System.Drawing.Font("Calibri", 14.26415F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnButtonTwo.Location = new System.Drawing.Point(421, 32);
+            this.btnButtonTwo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnButtonTwo.Name = "btnButtonTwo";
-            this.btnButtonTwo.Size = new System.Drawing.Size(166, 61);
+            this.btnButtonTwo.Size = new System.Drawing.Size(106, 36);
             this.btnButtonTwo.TabIndex = 1;
-            this.btnButtonTwo.Text = "Botão 2";
+            this.btnButtonTwo.Text = "Limpar";
             this.btnButtonTwo.UseVisualStyleBackColor = true;
+            this.btnButtonTwo.Click += new System.EventHandler(this.btnButtonTwo_Click);
             // 
             // btnButtonOne
             // 
-            this.btnButtonOne.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnButtonOne.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.btnButtonOne.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnButtonOne.Font = new System.Drawing.Font("Century Gothic", 14.26415F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnButtonOne.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnButtonOne.Image = global::PrjModulo1.Properties.Resources.button60;
-            this.btnButtonOne.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnButtonOne.Location = new System.Drawing.Point(97, 66);
+            this.btnButtonOne.Font = new System.Drawing.Font("Calibri", 14.26415F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnButtonOne.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnButtonOne.Location = new System.Drawing.Point(302, 32);
+            this.btnButtonOne.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnButtonOne.Name = "btnButtonOne";
-            this.btnButtonOne.Size = new System.Drawing.Size(169, 60);
+            this.btnButtonOne.Size = new System.Drawing.Size(111, 34);
             this.btnButtonOne.TabIndex = 0;
-            this.btnButtonOne.Text = "Botão";
-            this.btnButtonOne.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnButtonOne.Text = "Cadastrar";
             this.btnButtonOne.UseVisualStyleBackColor = false;
-            this.btnButtonOne.Click += new System.EventHandler(this.button1_Click);
+            this.btnButtonOne.Click += new System.EventHandler(this.btnButtonOne_Click);
             // 
-            // Form1
+            // lblNome
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.lblNome.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblNome.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNome.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.lblNome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblNome.Font = new System.Drawing.Font("Bodoni Bd BT", 14.26415F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblNome.Location = new System.Drawing.Point(40, 15);
+            this.lblNome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(210, 51);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "André Luís Moretto";
+            this.lblNome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCopy
+            // 
+            this.lblCopy.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCopy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopy.Location = new System.Drawing.Point(39, 66);
+            this.lblCopy.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCopy.Name = "lblCopy";
+            this.lblCopy.Size = new System.Drawing.Size(211, 49);
+            this.lblCopy.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.AcceptsReturn = true;
+            this.txtName.BackColor = System.Drawing.SystemColors.Control;
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtName.Location = new System.Drawing.Point(39, 152);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtName.MaxLength = 100;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(463, 29);
+            this.txtName.TabIndex = 4;
+            this.txtName.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtName_KeyUp);
+            this.txtName.Validating += new System.ComponentModel.CancelEventHandler(this.txtName_Validating);
+            // 
+            // lblNameTxtBox
+            // 
+            this.lblNameTxtBox.AutoSize = true;
+            this.lblNameTxtBox.Location = new System.Drawing.Point(36, 124);
+            this.lblNameTxtBox.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNameTxtBox.Name = "lblNameTxtBox";
+            this.lblNameTxtBox.Size = new System.Drawing.Size(55, 23);
+            this.lblNameTxtBox.TabIndex = 5;
+            this.lblNameTxtBox.Text = "Name";
+            // 
+            // lblNameOut
+            // 
+            this.lblNameOut.AutoSize = true;
+            this.lblNameOut.Location = new System.Drawing.Point(40, 190);
+            this.lblNameOut.Name = "lblNameOut";
+            this.lblNameOut.Size = new System.Drawing.Size(0, 23);
+            this.lblNameOut.TabIndex = 6;
+            // 
+            // lblNameList
+            // 
+            this.lblNameList.AutoSize = true;
+            this.lblNameList.Location = new System.Drawing.Point(35, 223);
+            this.lblNameList.Name = "lblNameList";
+            this.lblNameList.Size = new System.Drawing.Size(126, 23);
+            this.lblNameList.TabIndex = 7;
+            this.lblNameList.Text = "Lista de Nomes";
+            // 
+            // lstNames
+            // 
+            this.lstNames.BackColor = System.Drawing.SystemColors.Control;
+            this.lstNames.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstNames.FormattingEnabled = true;
+            this.lstNames.ItemHeight = 22;
+            this.lstNames.Location = new System.Drawing.Point(39, 250);
+            this.lstNames.Name = "lstNames";
+            this.lstNames.Size = new System.Drawing.Size(463, 156);
+            this.lstNames.TabIndex = 8;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 281);
+            this.ClientSize = new System.Drawing.Size(954, 586);
+            this.Controls.Add(this.lstNames);
+            this.Controls.Add(this.lblNameList);
+            this.Controls.Add(this.lblNameOut);
+            this.Controls.Add(this.lblNameTxtBox);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblCopy);
+            this.Controls.Add(this.lblNome);
             this.Controls.Add(this.btnButtonTwo);
             this.Controls.Add(this.btnButtonOne);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Font = new System.Drawing.Font("Calibri", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmMain";
+            this.Text = "Modulo1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,6 +171,13 @@
 
         private System.Windows.Forms.Button btnButtonOne;
         private System.Windows.Forms.Button btnButtonTwo;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblCopy;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblNameTxtBox;
+        private System.Windows.Forms.Label lblNameOut;
+        private System.Windows.Forms.Label lblNameList;
+        private System.Windows.Forms.ListBox lstNames;
     }
 }
 
--- PrjModulo1/Form1.cs
+++ PrjModulo1/Form1.cs
@@ -1,25 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PrjModulo1
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void btnButtonOne_Click(object sender, EventArgs e)
         {
-            btnButtonTwo.Text = "Alterado";
+            lblCopy.Text = lblNome.Text; 
+            //Attributes text to label copy from label name
+            lblCopy.BackColor = lblNome.BackColor; 
+            //Attributes the color of the control label Name to the Copy
+            lblNameOut.Text = txtName.Text;
+            //Atributes the text in the Textbox to the label NameOut
+            lstNames.Items.Add(txtName.Text);
+            txtName.Text = string.Empty;
+            txtName.Focus();
+        }
+        private void btnButtonTwo_Click(object sender, EventArgs e)
+        {
+            lblCopy.Text = string.Empty;
+            //Atributes a empy string or an "" string to the text property
+            lblCopy.BackColor = frmMain.DefaultBackColor; 
+            //Attributes the default color of main form to the label control
+            lblNameOut.Text = string.Empty;
+            txtName.Text = string.Empty;
+            //Atributes a empy string or an "" string to the text property
+        }
+        private void txtName_Validating(object sender, CancelEventArgs e)
+        {
+            if (txtName.Text.Length < 3)
+                MessageBox.Show("Nome < que 3 caracteres!");
+            /*If the length of the textbox is less them 3 chars an MessageBox pops up on screen
+            creating an information for the user.*/
+        }
+        private void txtName_KeyUp(object sender, KeyEventArgs e)
+        //The Class KeyEventArgs brings the key code pressed 
+        {
+            if (e.KeyCode == Keys.Enter) //Test if the "e" key is Enter
+            {
+                /*Calling the event Click of btnButtonOne, the constructor demands
+                 * Object Sender - in this case the name of the object that have the event
+                 * and the instance of EventArgs Class, just create new EventArgs()*/
+                btnButtonOne_Click(btnButtonOne, new EventArgs());
+            }
         }
     }
 }
