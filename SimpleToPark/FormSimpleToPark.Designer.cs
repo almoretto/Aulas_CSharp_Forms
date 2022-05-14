1972034960f3cfa0f67f9729da60bb92acf9721e
amoretto <amoretto@outlook.com.br>
2022-05-14 10:40:34 -0300

    Form Layout ok

--- SimpleToPark/FormSimpleToPark.Designer.cs
+++ SimpleToPark/FormSimpleToPark.Designer.cs
@@ -32,10 +32,11 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.btnInsertRegister = new System.Windows.Forms.Button();
+            this.btnConfig = new System.Windows.Forms.Button();
+            this.lblTotalValue = new System.Windows.Forms.Label();
             this.dtgDataView = new System.Windows.Forms.DataGridView();
+            this.lblHourValue = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
@@ -67,13 +68,13 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel2.ColumnCount = 1;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.lblTotalValue, 0, 1);
             this.tableLayoutPanel2.Controls.Add(this.dtgDataView, 0, 0);
             this.tableLayoutPanel2.Location = new System.Drawing.Point(155, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 2;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 84.04075F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.95925F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.34126F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.658744F));
             this.tableLayoutPanel2.Size = new System.Drawing.Size(982, 589);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
@@ -84,49 +85,59 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel3.ColumnCount = 1;
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel3.Controls.Add(this.button1, 0, 0);
-            this.tableLayoutPanel3.Controls.Add(this.button2, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.lblHourValue, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.btnInsertRegister, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnConfig, 0, 3);
             this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
-            this.tableLayoutPanel3.RowCount = 3;
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.62348F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 81.37652F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 94F));
+            this.tableLayoutPanel3.RowCount = 4;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.906882F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.09312F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 13F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 53F));
             this.tableLayoutPanel3.Size = new System.Drawing.Size(146, 589);
             this.tableLayoutPanel3.TabIndex = 1;
             // 
-            // button1
+            // btnInsertRegister
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.btnInsertRegister.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.Location = new System.Drawing.Point(3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(140, 33);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnInsertRegister.BackColor = System.Drawing.Color.DarkGray;
+            this.btnInsertRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInsertRegister.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnInsertRegister.Location = new System.Drawing.Point(3, 3);
+            this.btnInsertRegister.Name = "btnInsertRegister";
+            this.btnInsertRegister.Size = new System.Drawing.Size(140, 40);
+            this.btnInsertRegister.TabIndex = 0;
+            this.btnInsertRegister.Text = "Cadastrar";
+            this.btnInsertRegister.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // btnConfig
             // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.btnConfig.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button2.Location = new System.Drawing.Point(3, 497);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(140, 32);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnConfig.BackColor = System.Drawing.Color.DarkGray;
+            this.btnConfig.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfig.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnConfig.Location = new System.Drawing.Point(3, 538);
+            this.btnConfig.Name = "btnConfig";
+            this.btnConfig.Size = new System.Drawing.Size(140, 48);
+            this.btnConfig.TabIndex = 1;
+            this.btnConfig.Text = "Configurar";
+            this.btnConfig.UseVisualStyleBackColor = false;
             // 
-            // label1
+            // lblTotalValue
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 495);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 23);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lblTotalValue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTotalValue.AutoSize = true;
+            this.lblTotalValue.Location = new System.Drawing.Point(789, 566);
+            this.lblTotalValue.Name = "lblTotalValue";
+            this.lblTotalValue.Size = new System.Drawing.Size(190, 23);
+            this.lblTotalValue.TabIndex = 0;
+            this.lblTotalValue.Text = "Valor total: R$ 0,00";
+            this.lblTotalValue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // dtgDataView
             // 
@@ -143,8 +154,20 @@
             this.dtgDataView.ReadOnly = true;
             this.dtgDataView.RowHeadersWidth = 51;
             this.dtgDataView.RowTemplate.Height = 24;
-            this.dtgDataView.Size = new System.Drawing.Size(976, 489);
+            this.dtgDataView.Size = new System.Drawing.Size(976, 532);
             this.dtgDataView.TabIndex = 1;
+            // 
+            // lblHourValue
+            // 
+            this.lblHourValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblHourValue.AutoSize = true;
+            this.lblHourValue.Location = new System.Drawing.Point(3, 476);
+            this.lblHourValue.Name = "lblHourValue";
+            this.lblHourValue.Size = new System.Drawing.Size(140, 46);
+            this.lblHourValue.TabIndex = 2;
+            this.lblHourValue.Text = "Valor Hora: R$ 0,00";
+            this.lblHourValue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // FormSimpleToPark
             // 
@@ -155,7 +178,6 @@
             this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormSimpleToPark";
@@ -164,6 +186,7 @@
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
             this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dtgDataView)).EndInit();
             this.ResumeLayout(false);
 
@@ -173,11 +196,12 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTotalValue;
         private System.Windows.Forms.DataGridView dtgDataView;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnInsertRegister;
+        private System.Windows.Forms.Button btnConfig;
+        private System.Windows.Forms.Label lblHourValue;
     }
 }
 
