9a0c1283314a5e69f842a3b7feabf10c010b7c5d
amoretto <amoretto@outlook.com.br>
2022-05-14 09:40:37 -0300

    Initial Config

--- SimpleToPark/FormSimpleToPark.Designer.cs
+++ SimpleToPark/FormSimpleToPark.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SimpleToPark
 {
-    partial class Form1
+    partial class FormSimpleToPark
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormSimpleToPark
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FormSimpleToPark";
+            this.Text = "FormSimpleToPark";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
